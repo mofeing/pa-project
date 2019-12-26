2f96f8bee9983d9d067ebe8af868aded783c2525
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2019-12-26 19:59:32 +0100

    Test I-Cache

---
+++ test/mmu/icache.cpp
@@ -0,0 +1,78 @@
+#include <verilated_vcd_c.h>
+#include <verilated.h>
+#include "Vicache_directmap.h"
+#include <memory>
+
+int main(int argc, char const *argv[])
+{
+	Verilated::commandArgs(argc, argv);
+
+	// Instantiate module
+	std::unique_ptr<Vicache_directmap> mod(new Vicache_directmap);
+
+	// Activate waveform tracing
+	Verilated::traceEverOn(true);
+	std::unique_ptr<VerilatedVcdC> tracer(new VerilatedVcdC);
+	mod->trace(tracer.get(), 0);
+	tracer->open("icache_directmap.vcd");
+
+	// Tests
+	vluint64_t time = 0;
+	auto &clk = mod->clk;
+	auto &rst = mod->rst;
+
+	auto tick = [&]() {
+		clk = 1;
+		mod->eval();
+		tracer->dump(time++);
+
+		clk = 0;
+		mod->eval();
+		tracer->dump(time++);
+	};
+
+	/// Reset
+	rst = 1;
+	clk = 1;
+	mod->eval();
+	tracer->dump(time++);
+
+	tick();
+
+	clk = 0;
+	rst = 0;
+	mod->eval();
+	tracer->dump(time++);
+
+	/// Check empty cache, different idx (must miss, two mem requests)
+	mod->thread = 0;
+	mod->paddr = 0x1000;
+	tick();
+
+	mod->thread = 1;
+	mod->paddr = 0x2000;
+	tick();
+
+	/// Check waiting cacheling (must miss, but must not issue request to memory)
+	mod->thread = 2;
+	mod->paddr = 0x1000;
+	tick();
+
+	/// Answer from memory
+	mod->thread = 3;
+	mod->paddr = 0x2000;
+	mod->mem_rec_en = 1;
+	mod->mem_rec_addr = 0x2000;
+	for (auto i = 0; i < 4; i++)
+		mod->mem_rec_cacheline[i] = 0xFFFFFFFF;
+	tick();
+
+	mod->thread = 4;
+	mod->mem_rec_en = 0;
+	tick();
+
+	mod->final();
+	tracer->close();
+
+	return 0;
+}
