1c8eb42f9c0bb817492fa6cdb74a0a8dcdb03577
Trucaoman <marcoaguado95@gmail.com>
2019-12-30 18:43:15 +0100

    Added Hazard Detecton Unit and DTLB test files

---
+++ test/control/hzu.cpp
@@ -0,0 +1,120 @@
+#include <verilated_vcd_c.h>
+#include <verilated.h>
+#include "Vhzu.h"
+#include <memory>
+
+int main(int argc, char const *argv[])
+{
+	Verilated::commandArgs(argc, argv);
+
+	// Instantiate module
+	std::unique_ptr<Vhzu> mod(new Vhzu);
+
+	// Activate waveform tracing
+	Verilated::traceEverOn(true);
+	std::unique_ptr<VerilatedVcdC> tracer(new VerilatedVcdC);
+	mod->trace(tracer.get(), 0);
+	tracer->open("hzu.vcd");
+
+	// Tests
+	vluint64_t time = 0;
+	auto &clk = mod->clk;
+	auto &rst = mod->rst;
+
+    auto tick = [&]() {
+		clk = 1;
+		mod->eval();
+		tracer->dump(time++);
+
+		clk = 0;
+		mod->eval();
+		tracer->dump(time++);
+	};
+
+    /// Reset hzu
+    rst = 1;
+    tick();
+    tick();
+    tick();
+    rst = 0;
+
+    //MISSES
+
+    //If itlb_miss
+    i_tlb_miss = 1;
+    i_cache_miss = 0;
+    tick();
+
+    //If icache_miss
+    i_tlb_miss = 0;
+    i_cache_miss = 1;
+    tick();
+
+    //DRACE
+
+    //If drace of any instruction src1 = previous instruction dst
+    mod->instruction = Vcommon_opcode::add << 25 + 4 << 20 + 1 << 15 + 2 << 10; //Instruccion con dst R4
+    tick();
+    mod->instruction = Vcommon_opcode::add << 25 + 5 << 20 + 4 << 15 + 2 << 10; //Instruccion con src1 R4
+    tick();
+
+    //If drace of instruction R src2 = previous instruction dst
+    mod->instruction = Vcommon_opcode::add << 25 + 6 << 20 + 1 << 15 + 2 << 10; //Instruccion con dst R6
+    tick();
+    mod->instruction = Vcommon_opcode::add << 25 + 7 << 20 + 1 << 15 + 6 << 10; //Instruccion con src2 R6
+    tick();
+
+    //NO STORE/LOAD AFTER STORE
+
+    //If Load word after Store word
+    mod->instruction = Vcommon_opcode::stw << 25;
+    tick();
+    mod->instruction = Vcommon_opcode::ldw << 25;
+
+    //If Store word after Store word
+    mod->instruction = Vcommon_opcode::stw << 25;
+    tick();
+    mod->instruction = Vcommon_opcode::stw << 25;
+    tick();
+
+    //If Load byte after store word
+    mod->instruction = Vcommon_opcode::stw << 25;
+    tick();
+    mod->instruction = Vcommon_opcode::ldb << 25;
+    tick();
+
+    //If Store byte after store word
+    mod->instruction = Vcommon_opcode::stw << 25;
+    tick();
+    mod->instruction = Vcommon_opcode::stb << 25;
+    tick();
+
+    //If Load word after Store byte
+    mod->instruction = Vcommon_opcode::stb << 25;
+    tick();
+    mod->instruction = Vcommon_opcode::ldw << 25;
+
+    //If Store word after Store byte
+    mod->instruction = Vcommon_opcode::stb << 25;
+    tick();
+    mod->instruction = Vcommon_opcode::stw << 25;
+    tick();
+
+    //If Load byte after store byte
+    mod->instruction = Vcommon_opcode::stb << 25;
+    tick();
+    mod->instruction = Vcommon_opcode::ldb << 25;
+    tick();
+
+    //If Store byte after store byte
+    mod->instruction = Vcommon_opcode::stb << 25;
+    tick();
+    mod->instruction = Vcommon_opcode::stb << 25;
+    tick();
+
+	tracer->dump(time++);
+	mod->final();
+	tracer->close();
+
+	return 0;
+}
---
+++ test/mmu/dtlb.cpp
@@ -0,0 +1,145 @@
+#include <verilated_vcd_c.h>
+#include <verilated.h>
+#include "Vdtlb.h"
+#include <memory>
+
+int main(int argc, char const *argv[])
+{
+	Verilated::commandArgs(argc, argv);
+
+	// Instantiate module
+	std::unique_ptr<Vdtlb> mod(new Vdtlb);
+
+	// Activate waveform tracing
+	Verilated::traceEverOn(true);
+	std::unique_ptr<VerilatedVcdC> tracer(new VerilatedVcdC);
+	mod->trace(tracer.get(), 0);
+	tracer->open("dtlb.vcd");
+
+	// Tests
+	vluint64_t time = 0;
+	auto &clk = mod->clk;
+	auto &rst = mod->rst;
+
+	auto tick = [&]() {
+		clk = 1;
+		mod->eval();
+		tracer->dump(time++);
+
+		clk = 0;
+		mod->eval();
+		tracer->dump(time++);
+	};
+
+	/// Reset dTLB
+	rst = 1;
+	clk = 1;
+	mod->eval();
+	tracer->dump(time++);
+
+	clk = 0;
+	rst = 0;
+	mod->eval();
+	tracer->dump(time++);
+
+	/// Check translation in supervisor mode (bypass)
+	mod->mode = 1;
+	mod->vaddr = 0x0;
+	tick();
+
+	mod->vaddr = 0x1000;
+	tick();
+
+	mod->vaddr = 0x22222;
+	tick();
+
+	/// Check translation in user mode (must miss)
+	mod->mode = 0;
+	mod->vaddr = 0x0;
+	tick();
+
+	mod->vaddr = 0x1000;
+	tick();
+
+	/// TLBwrite correct
+    mod->mode = 1
+	mod->write_en = 1;
+    mod->is_valid = 1;
+    mod->flag_mem = 1;
+	mod->write_vpn = 0x1000 >> 6;
+	mod->write_ppn = 0x20;
+	tick();
+
+    // TLBwrite fail (not supervisor mode)
+	mod->mode = 0
+	mod->write_en = 1;
+    mod->is_valid = 1;
+    mod->flag_mem = 1;
+	mod->write_vpn = 0x1000 >> 6;
+	mod->write_ppn = 0x20;
+	tick();
+
+    // TLBwrite fail (not write enabled)
+	mod->mode = 1
+	mod->write_en = 0;
+    mod->is_valid = 1;
+    mod->flag_mem = 1;
+	mod->write_vpn = 0x1000 >> 6;
+	mod->write_ppn = 0x20;
+	tick();
+
+    // TLBwrite fail (not valid)
+	mod->mode = 1
+	mod->write_en = 1;
+    mod->is_valid = 0;
+    mod->flag_mem = 1;
+	mod->write_vpn = 0x1000 >> 6;
+	mod->write_ppn = 0x20;
+	tick();
+
+    // TLBwrite fail (not flag_mem)
+	mod->mode = 1
+	mod->write_en = 1;
+    mod->is_valid = 1;
+    mod->flag_mem = 0;
+	mod->write_vpn = 0x1000 >> 6;
+	mod->write_ppn = 0x20;
+	tick();
+
+    //RETURN PHISICAL ADDRESS
+    //First it has it *
+    mod->write_vpn = 0x1000 >> 6;
+	mod->write_ppn = 0x20;
+
+    //Return phisical address (It has it and correct)
+    mod->mode = 0;
+    mod->is_valid = 1;
+    mod->flag_mem = 1;
+    mod->vaddr = ; //Direccion a pedir
+    mod->is_valid = 1;
+	tick();
+
+    //Return phisical address (It has it but Not valid)
+
+
+
+    //Return phisical address (It has it but no flag_mem)
+
+    
+    //Does not have it *
+
+    //Cache miss
+    mod->mode = 0;
+    mod->is_valid = 1;
+    mod->flag_mem = 1;
+    mod->vaddr = ; //Direccion a pedir
+    mod->is_valid = 1;
+	tick();
+
+
+	tick();
+	mod->final();
+	tracer->close();
+
+	return 0;
+}
