acc049a944c4b30a737dfd7b3a2f3768d8cd6874
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2019-12-31 21:12:04 +0100

    Scheduler force thread on exception

--- src/control/scheduler.sv
+++ src/control/scheduler.sv
@@ -6,11 +6,16 @@ module scheduler_roundrobin
 (
 	input   			clk,
 	input   			rst,
-	output  threadid_t	thread
+	output  threadid_t	thread,
+
+	input logic			exc_en,
+	input threadid_t	exc_thread
 );
 	always_ff @(posedge clk) begin
-		if (rst)	thread <= 0;
-		else		thread <= (thread + 1) % 8;
+		if (rst)	thread = 0;
+		else begin
+			thread = (exc_en) ? exc_thread : (thread + 1) % 8;
+		end
 	end
 endmodule
 
@@ -21,32 +26,35 @@ module scheduler_priority
 	input				rst,
 
 	output	threadid_t	thread,
-	input	logic		stalled[n_threads-1:0]
+	input	logic		stalled[n_threads-1:0],
+
+	input logic			exc_en,
+	input threadid_t	exc_thread
 );
 	threadid_t guess;
 	integer i;
 
-
 	// Start first thread on reset
-	always @(negedge rst)
-		guess = 0;
-
 	always @(posedge clk) begin
 		if (rst)
 			guess = 0;
 		else begin
-			// Guess next thread is not stalled
-			thread = guess;
+			if (~exc_en) begin
+				// Guess next thread is not stalled
+				thread = guess;
 
-			// Skip to next non-stalled thread
-			for (i = n_threads-1; i >= 1; i = i - 1) begin
-				integer j = (i + guess) % n_threads;
-				if (stalled[j] == 0)
-					thread = j;
+				// Skip to next non-stalled thread
+				for (i = n_threads-1; i >= 1; i = i - 1) begin
+					integer j = (i + guess) % n_threads;
+					if (stalled[j] == 0)
+						thread = j;
+				end
+
+				// Update next guess
+				guess = (thread + 1) % n_threads;
 			end
-
-			// Update next guess
-			guess = (thread + 1) % n_threads;
+			else
+				thread = exc_thread;
 		end
 	end
 endmodule
