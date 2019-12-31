5a35ae2b0d526fe104f30b8bc8c7f74181862951
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2019-12-31 16:22:02 +0100

    Indent hzu using tabs

--- test/control/hzu.cpp
+++ test/control/hzu.cpp
@@ -21,7 +21,7 @@ int main(int argc, char const *argv[])
 	auto &clk = mod->clk;
 	auto &rst = mod->rst;
 
-    auto tick = [&]() {
+	auto tick = [&]() {
 		clk = 1;
 		mod->eval();
 		tracer->dump(time++);
@@ -31,88 +31,88 @@ int main(int argc, char const *argv[])
 		tracer->dump(time++);
 	};
 
-    /// Reset hzu
-    rst = 1;
-    tick();
-    tick();
-    tick();
-    rst = 0;
+	/// Reset hzu
+	rst = 1;
+	tick();
+	tick();
+	tick();
+	rst = 0;
 
-    //MISSES
+	//MISSES
 
-    //If itlb_miss
-    i_tlb_miss = 1;
-    i_cache_miss = 0;
-    tick();
+	//If itlb_miss
+	itlb_miss = 1;
+	i_cache_miss = 0;
+	tick();
 
-    //If icache_miss
-    i_tlb_miss = 0;
-    i_cache_miss = 1;
-    tick();
+	//If icache_miss
+	i_tlb_miss = 0;
+	i_cache_miss = 1;
+	tick();
 
-    //DRACE
+	//DRACE
 
-    //If drace of any instruction src1 = previous instruction dst
-    mod->instruction = Vcommon_opcode::add << 25 + 4 << 20 + 1 << 15 + 2 << 10; //Instruccion con dst R4
-    tick();
-    mod->instruction = Vcommon_opcode::add << 25 + 5 << 20 + 4 << 15 + 2 << 10; //Instruccion con src1 R4
-    tick();
+	//If drace of any instruction src1 = previous instruction dst
+	mod->instruction = Vcommon_opcode::add << 25 + 4 << 20 + 1 << 15 + 2 << 10; //Instruccion con dst R4
+	tick();
+	mod->instruction = Vcommon_opcode::add << 25 + 5 << 20 + 4 << 15 + 2 << 10; //Instruccion con src1 R4
+	tick();
 
-    //If drace of instruction R src2 = previous instruction dst
-    mod->instruction = Vcommon_opcode::add << 25 + 6 << 20 + 1 << 15 + 2 << 10; //Instruccion con dst R6
-    tick();
-    mod->instruction = Vcommon_opcode::add << 25 + 7 << 20 + 1 << 15 + 6 << 10; //Instruccion con src2 R6
-    tick();
+	//If drace of instruction R src2 = previous instruction dst
+	mod->instruction = Vcommon_opcode::add << 25 + 6 << 20 + 1 << 15 + 2 << 10; //Instruccion con dst R6
+	tick();
+	mod->instruction = Vcommon_opcode::add << 25 + 7 << 20 + 1 << 15 + 6 << 10; //Instruccion con src2 R6
+	tick();
 
-    //NO STORE/LOAD AFTER STORE
+	//NO STORE/LOAD AFTER STORE
 
-    //If Load word after Store word
-    mod->instruction = Vcommon_opcode::stw << 25;
-    tick();
-    mod->instruction = Vcommon_opcode::ldw << 25;
-    tick();
+	//If Load word after Store word
+	mod->instruction = Vcommon_opcode::stw << 25;
+	tick();
+	mod->instruction = Vcommon_opcode::ldw << 25;
+	tick();
 
-    //If Store word after Store word
-    mod->instruction = Vcommon_opcode::stw << 25;
-    tick();
-    mod->instruction = Vcommon_opcode::stw << 25;
-    tick();
+	//If Store word after Store word
+	mod->instruction = Vcommon_opcode::stw << 25;
+	tick();
+	mod->instruction = Vcommon_opcode::stw << 25;
+	tick();
 
-    //If Load byte after store word
-    mod->instruction = Vcommon_opcode::stw << 25;
-    tick();
-    mod->instruction = Vcommon_opcode::ldb << 25;
-    tick();
+	//If Load byte after store word
+	mod->instruction = Vcommon_opcode::stw << 25;
+	tick();
+	mod->instruction = Vcommon_opcode::ldb << 25;
+	tick();
 
-    //If Store byte after store word
-    mod->instruction = Vcommon_opcode::stw << 25;
-    tick();
-    mod->instruction = Vcommon_opcode::stb << 25;
-    tick();
+	//If Store byte after store word
+	mod->instruction = Vcommon_opcode::stw << 25;
+	tick();
+	mod->instruction = Vcommon_opcode::stb << 25;
+	tick();
 
-    //If Load word after Store byte
-    mod->instruction = Vcommon_opcode::stb << 25;
-    tick();
-    mod->instruction = Vcommon_opcode::ldw << 25;
-    tick();
-    
-    //If Store word after Store byte
-    mod->instruction = Vcommon_opcode::stb << 25;
-    tick();
-    mod->instruction = Vcommon_opcode::stw << 25;
-    tick();
+	//If Load word after Store byte
+	mod->instruction = Vcommon_opcode::stb << 25;
+	tick();
+	mod->instruction = Vcommon_opcode::ldw << 25;
+	tick();
 
-    //If Load byte after store byte
-    mod->instruction = Vcommon_opcode::stb << 25;
-    tick();
-    mod->instruction = Vcommon_opcode::ldb << 25;
-    tick();
+	//If Store word after Store byte
+	mod->instruction = Vcommon_opcode::stb << 25;
+	tick();
+	mod->instruction = Vcommon_opcode::stw << 25;
+	tick();
 
-    //If Store byte after store byte
-    mod->instruction = Vcommon_opcode::stb << 25;
-    tick();
-    mod->instruction = Vcommon_opcode::stb << 25;
-    tick();
+	//If Load byte after store byte
+	mod->instruction = Vcommon_opcode::stb << 25;
+	tick();
+	mod->instruction = Vcommon_opcode::ldb << 25;
+	tick();
+
+	//If Store byte after store byte
+	mod->instruction = Vcommon_opcode::stb << 25;
+	tick();
+	mod->instruction = Vcommon_opcode::stb << 25;
+	tick();
 
 	tracer->dump(time++);
 	mod->final();
