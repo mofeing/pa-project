ec8c50b194bc3dd3d71ce45530906c2b87d3bb1b
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-01-04 19:45:00 +0100

    Rewrite dcache module

--- src/mmu/dcache.sv
+++ src/mmu/dcache.sv
@@ -23,6 +23,7 @@ module dcache_directmap
 	input	threadid_t				thread,
 
 	input	pptr_t					paddr,
+	input	logic					isvalid,
 
 	output	logic					miss,
 	output	word_t					data,
@@ -55,25 +56,51 @@ module dcache_directmap
 );
 	dcache_entry_t entry[n_cachelines];
 	dcache_listener_t listener[n_threads];
+	idx_t req_idx, rec_idx;
+	tag_t req_tag, rec_tag;
+
+	always_comb begin
+		req_idx = paddr.fields.idx;
+		req_tag = paddr.fields.tag;
+		rec_idx = mem_rec_addr.fields.idx;
+		rec_tag = mem_rec_addr.fields.tag;
+
+		miss = ~(entry[req_idx].valid && entry[req_idx].tag == req_tag) || dtlb_miss;
+		data = entry[req_idx].data.words[paddr.fields.offset];
+	end
 
 	always_ff @(posedge clk) begin
-		// Defaults
-		miss <= 0;
-		mem_req_ren <= 0;
-		mem_req_wen <= 0;
-
-		if (rst) begin : reset
+		if (rst) begin
 			foreach (entry[i]) entry[i].valid = 0;
 			foreach (listener[i]) listener[i].valid = 0;
 		end
-		else begin : active
+		else begin
+			// Commit store to cacheline after passing the committer
+			if (store_en) begin
+				idx_t st_idx = store_addr.fields.idx;
+				byte_offset_t st_byte_offset = store_addr.fields.offset;
+
+				// Mark entry as dirty
+				entry[st_idx].dirty = 1;
+
+				if (store_isbyte)
+					entry[st_idx].data.bytes[st_byte_offset] = store_data[7:0];
+				else
+					entry[st_idx].data.words[st_byte_offset[3:2]] = store_data;
+			end
+
 			// Receive from memory
-			idx_t rec_idx = mem_rec_addr.fields.idx;
-			tag_t rec_tag = mem_rec_addr.fields.tag;
-			if (mem_rec_en && entry[rec_idx].req_tag == rec_tag) begin : mem_receive
+			if (mem_rec_en && entry[rec_idx].req_tag == rec_tag) begin
+				// Commit cacheline to memory if dirty
+				if (entry[rec_idx].valid && entry[rec_idx].dirty) begin
+					mem_req_wen <= 1;
+					mem_req_waddr <= {entry[rec_idx].tag, rec_idx, {$bits(byte_offset_t){1'b0}}};
+					mem_req_wcacheline <= entry[rec_idx].data;
+				end
+
 				// Save cacheline
 				entry[rec_idx].valid = 1;
-				entry[rec_idx].tag = entry[rec_idx].req_tag;
+				entry[rec_idx].tag = rec_tag;
 				entry[rec_idx].data = mem_rec_cacheline;
 				entry[rec_idx].waiting = 0;
 				entry[rec_idx].dirty = 0;
@@ -82,71 +109,28 @@ module dcache_directmap
 				foreach (listener[i])
 					if (listener[i].valid == 1 && listener[i].idx == rec_idx) begin
 						stalled[i] <= 0;
-						listener[i].valid = 0;
+						listener[i].valid <= 0;
 					end
 			end
 
-			// Commit store after passing the committer
-			if (store_en) begin : store_receive
-				idx_t st_idx = store_addr.fields.idx;
-				byte_offset_t st_byte_offset = store_addr.fields.offset;
-
-				// Mark entry as dirty
-				entry[st_idx].dirty = 1;
-
-				if (store_isbyte) begin
-					entry[st_idx].data.bytes[st_byte_offset] = store_data[7:0];
-				end
-				else begin
-					entry[st_idx].data.words[st_byte_offset[3:2]] = store_data;
-				end
-			end
-
-			// Proceed if no D-TLB miss
-			if (flag_mem && ~dtlb_miss) begin : proceed
-				idx_t req_idx = paddr.fields.idx;
-				tag_t req_tag = paddr.fields.tag;
-
-				if (~flag_store) begin // load
-					// Read on hit
-					if (entry[req_idx].valid && entry[req_idx].tag == req_tag) begin
-						data <= (flag_isbyte)
-							? {24'b0, entry[req_idx].data.bytes[paddr.fields.offset]}
-							: entry[req_idx].data.words[paddr.fields.offset[3:2]];
-					end
-					else
-						miss <= 1;
-				end
-				else begin // store
-					// Design artifact: cannot do store if cacheline can be replaced on the next cycle
-					if (~entry[req_idx].valid || entry[req_idx].tag != req_tag || entry[req_idx].waiting)
-						miss <= 1;
-				end
-
-				// Request cacheline to memory if miss and entry is not waiting other cacheline
-				if (miss && (~entry[req_idx].waiting || ~entry[req_idx].valid)) begin : mem_request
-					// Request cacheline to memory
+			// Request memory on miss
+			mem_req_ren <= 0;
+			if (flag_mem && isvalid && ~dtlb_miss && miss) begin
+				// NOTE Can only request memory if entry is not waiting for memory
+				if (~entry[req_idx].waiting) begin
 					mem_req_ren <= 1;
-					mem_req_raddr <= paddr;
-
-					// Flush cacheline to memory if dirty
-					if (entry[req_idx].valid && entry[req_idx].dirty) begin
-						mem_req_wen <= 1;
-						mem_req_waddr <= {entry[req_idx].tag, req_idx, 4'b0};
-						mem_req_wcacheline <= entry[req_idx].data;
-					end
-
-					// Stall thread
-					listener[thread].valid <= 1;
-					listener[thread].idx <= req_idx;
-					stalled[thread] <= 1;
+					mem_req_raddr <= {req_tag, req_idx, {$bits(byte_offset_t){1'b0}}};
 
 					// Set cacheline on waiting state
-					entry[req_idx].waiting <= 1;
-					entry[req_idx].req_tag <= req_tag;
+					entry[req_idx].waiting = 1;
+					entry[req_idx].req_tag = req_tag;
 				end
+
+				// Stall thread
+				listener[thread].valid <= 1;
+				listener[thread].idx <= req_idx;
+				stalled[thread] <= 1;
 			end
 		end
 	end
-
 endmodule
