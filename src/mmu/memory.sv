a676c2c77a35a0133eb761783932713b99aa19ab
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-01-04 01:37:36 +0100

    Fix loading of program to memory

--- src/mmu/memory.sv
+++ src/mmu/memory.sv
@@ -4,6 +4,7 @@ import common::*;
 module memory
 (
 	input	logic		clk,
+	input	logic		rst,
 
 	input	logic		req_ren,
 	input	pptr_t		req_raddr,
@@ -16,19 +17,27 @@ module memory
 	output	pptr_t		rec_addr,
 	output	cacheline_t	rec_cacheline
 );
-	cacheline_t data [2**($bits(pptr_t)-$bits(cacheline_t))]; // should be 1MB
+	cacheline_t data [2**($bits(tag_t) + $bits(idx_t))]; // should be 1MB
 
 	always @(posedge clk) begin
 		rec_en <= 0;
 
-		if (req_wen) begin
-			data[{req_waddr.fields.tag, req_waddr.fields.idx}] <= req_wcacheline;
+		if (rst) begin
+			integer file;
+			file = $fopen("programs/buffer_sum.bin", "r");
+			$fread(data, file);
+			$fclose(file);
 		end
+		else begin
+			if (req_wen) begin
+				data[{req_waddr.fields.tag, req_waddr.fields.idx}] <= req_wcacheline;
+			end
 
-		if (req_ren) begin
-			rec_en <= 1;
-			rec_addr <= req_raddr;
-			rec_cacheline <= data[{req_raddr.fields.tag, req_raddr.fields.idx}];
+			if (req_ren) begin
+				rec_en <= 1;
+				rec_addr <= req_raddr;
+				rec_cacheline <= data[{req_raddr.fields.tag, req_raddr.fields.idx}];
+			end
 		end
 	end
 
