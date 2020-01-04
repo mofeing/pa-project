4bad619e943d2f1df0f1cca34b413c08b63f9cd6
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-01-04 18:56:47 +0100

    Rewrite hzu module

--- src/control/hzu.sv
+++ src/control/hzu.sv
@@ -29,62 +29,39 @@ module hzu
 
 	output	logic		isvalid
 );
-	history_entry_t history [8];
-	logic drace;
+	history_entry_t history [n_threads-1];
 	logic issrc2kind;
 
-	assign isvalid = history[0].valid;
-
-	always_ff @(posedge clk) begin
-		if (rst) begin
-			foreach (history[i])
-				history[i].valid = 0;
-
-		end
+	always_comb begin
+		if (rst || itlb_miss || icache_miss) isvalid = 0;
 		else begin
-			// Move up history
-			for (int i = 0; i < $size(history)-1; i++)
-				history[i+1] = history[i];
+			isvalid = 1;
+			issrc2kind = has_src2(instr.op);
 
-			// Save instruction in history
-			history[0].valid = 1; // speculative, overwritten in the end
-			history[0].thread = thread;
-			history[0].instr = instr;
-
-			// Check for data-race
-			drace <= 0;
-			issrc2kind <= has_src2(history[0].instr.op);
-			for (int i = 1; i < $size(history); i++)
+			foreach (history[i])
 				if (history[i].valid && history[i].thread == thread && (
-						// dst and src1 do not clash
-						(has_dst(history[i].instr.op) && history[0].instr.fields.r.src1 == history[i].instr.fields.r.dst)
-						// dst and src2 do not clash
-						|| (has_dst(history[i].instr.op) && issrc2kind && history[0].instr.fields.r.src2 == history[i].instr.fields.r.dst)
-					)
-				)
-					drace <= 1;
-
-			// Design artifact: no store/load after store
-			// if (history[1].valid
-			// 	&& (
-			// 		history[1].instr.op == opcode::stb
-			// 		|| history[1].instr.op == opcode::stw
-			// 		)
-			// 	&& (
-			// 		history[0].instr.op == opcode::stb
-			// 		|| history[0].instr.op == opcode::stw
-			// 		|| history[0].instr.op == opcode::ldb
-			// 		|| history[0].instr.op == opcode::ldw
-			// 		)
-			// 	)
-			// 	isvalid = 0;
-
-			// Commit isvalid if no I-TLB exception and I-cache miss
-			if (itlb_miss || icache_miss || drace) begin
-				history[0].valid = 0;
-			end
+					// dst and src1 do not clash
+					(has_dst(history[i].instr.op) && history[0].instr.fields.r.src1 == history[i].instr.fields.r.dst)
+					// dst and src2 do not clash
+					|| (has_dst(history[i].instr.op) && issrc2kind && history[0].instr.fields.r.src2 == history[i].instr.fields.r.dst)
+				)) begin
+					isvalid = 0;
+					break;
+				end
 		end
 	end
 
-
+	always_ff @(posedge clk) begin
+		if (rst)
+			foreach (history[i])
+				history[i].valid = 0;
+		else begin
+			// Update history
+			history[0].valid <= isvalid;
+			history[0].thread <= thread;
+			history[0].instr <= instr;
+			for (int i = 1; i < $size(history); i++)
+				history[i] <= history[i-1];
+		end
+	end
 endmodule
